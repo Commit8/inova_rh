7a1c02cdb0aac4f13bca2f59041ccb7179b02589
Gabriel Messias <gabrielmessias@outlook.com>
2025-09-26 13:29:41 -0300

    app.module.ts concluido

--- inova_rh/src/app.module.ts
+++ inova_rh/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ColaboradorModule } from './colaboradores/colaborador.module';
+import { Colaborador } from './colaboradores/entities/colaborador.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_inovarh',
+      entities: [Colaborador],
+      synchronize: true,
+    }),
+    ColaboradorModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
