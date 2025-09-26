ceaa6685d5fe1c075e7e63edbd0c39c3bd50636a
Dan <dandara.oliveira.n@gmail.com>
2025-09-26 13:15:14 -0300

    Entity colaborador adicionado

---
+++ inova_rh/src/colaboradores/entities/colaborador.entity.ts
@@ -0,0 +1,24 @@
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'tb_colaboradores' })
+export class Colaborador {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  nome: string;
+
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  cargo: string;
+
+  @IsNotEmpty()
+  @Column({ nullable: false })
+  admissao: Date;
+
+  @IsNotEmpty()
+  @Column({ type: 'decimal', precision: 6, scale: 2, nullable: false })
+  salario: number;
+}
