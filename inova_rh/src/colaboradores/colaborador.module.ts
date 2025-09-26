5113efaf32b35a0f368b0e75895e3fef47bf3571
L4ish <laish_rodrigues@hotmail.com>
2025-09-26 14:43:22 -0300

    sistema rodando lisinho

--- inova_rh/src/colaboradores/colaborador.module.ts
+++ inova_rh/src/colaboradores/colaborador.module.ts
@@ -2,8 +2,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ColaboradorService } from './service/colaborador.service';
 import { Colaborador } from './entities/colaborador.entity';
 import { ColaboradorController } from './controllers/colaborador.controller';
+import { Module } from '@nestjs/common';
 
-@module({
+@Module({
   imports: [TypeOrmModule.forFeature([Colaborador])],
   providers: [ColaboradorService],
   controllers: [ColaboradorController],
